0809cd988a4abd931f7f9ac01ab1db1186f80567
julio cesar aguero <julio.aguero.gomez@gmail.com>
2022-04-05 12:20:21 -0500

    practica I

---
+++ app.js
@@ -0,0 +1,4 @@
+const {frutas,precios} = require('./frutas')
+console.log("Michi")
+frutas.forEach((item) => console.log(item));
+console.log(precios)
---
+++ frutas.js
@@ -0,0 +1,3 @@
+const frutas = ['sandia','fresaA','cereza']
+const precios = [100,200];
+module.exports = {frutas,precios};
